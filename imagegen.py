a9a5610e205e10959b13b11a158ba75fda8865fd
korrakrit gumnarai <110856608+paowick@users.noreply.github.com>
2025-05-01 14:58:02 +0700

    Update imagegen.py

--- imagegen.py
+++ imagegen.py
@@ -4,25 +4,25 @@ client = OpenAI(api_key="") # Add your API key here
 
 def generate_image(prompt):
     try:
-        #response = client.images.generate(
-        #    model="dall-e-3",
-        #    prompt="genarated this image " + prompt,
-        #    size="1024x1024",
-        #    quality="standard",
-        #    n=1,
-        #)
-        return "https://i.kym-cdn.com/entries/icons/original/000/036/070/cover5.jpg"
-        #return response.data[0].url
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt="genarated this image " + prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        #return "https://i.kym-cdn.com/entries/icons/original/000/036/070/cover5.jpg"
+        return response.data[0].url
     except Exception as e:
         print("Error generating image:",e)
         print("Try again with DALL-E 2")
-        #response = client.images.generate(
-           # model="dall-e-2",
-           # prompt="genarated this image " + prompt,
-           # size="1024x1024",
-           # quality="standard",
-           # n=1,
-        #)
-        return "https://i.kym-cdn.com/entries/icons/original/000/036/070/cover5.jpg"
-        #return response.data[0].url
+        response = client.images.generate(
+            model="dall-e-2",
+            prompt="genarated this image " + prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        #return "https://i.kym-cdn.com/entries/icons/original/000/036/070/cover5.jpg"
+        return response.data[0].url
 
