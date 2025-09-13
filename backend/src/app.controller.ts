23e7db641e76dcc835a256f97e8388d739cf6f9c
paowick <korn2k9@gmail.com>
2025-09-13 14:56:37 +0700

    feat: add backend nestjs , auth system with pasport

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+  ) { }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/logout')
+  async logout(@Request() req) {
+    return req.logout();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+
+
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
